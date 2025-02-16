33e79d435188658d46a8380f4a1ea36640f0579d
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2025-02-16 20:43:06 +0530

    generate function

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-
 import streamlit as st
 from dotenv import load_dotenv
 import google.generativeai as genai
@@ -8,6 +6,20 @@ import os
 # --------------Utils-------------------
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE-API-KEY"))
+
+
+# --------------Functions--------------
+def generate_gemini_content(transcript_text, prompt):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt + transcript_text)
+    return response.text
+
+
+prompt = """
+You are youtube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+within 250 words. The transcript text will be appended here : 
+"""
 
 # ----------------UI-----------------
 
